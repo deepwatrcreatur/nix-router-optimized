97a3cbd4ed2ebd8cb99948875279b8dbf903528e
Anwer Khan <deepwatrcreatur@gmail.com>
2026-05-20 04:41:53 -0400

    test(router-zones): extend assertion coverage

--- tests/router-zones.nix
+++ tests/router-zones.nix
@@ -159,4 +159,88 @@
       };
     }
   ];
+
+  router-zones-empty-zones-fails = eval.mkNixosEvalFailureCheck "router-zones-empty-zones" [
+    self.nixosModules.router-firewall
+    self.nixosModules.router-zones
+    {
+      services.router-firewall.enable = true;
+      services.router-zones = {
+        enable = true;
+        zones = { };
+      };
+    }
+  ];
+
+  router-zones-firewall-disabled-fails = eval.mkNixosEvalFailureCheck "router-zones-firewall-disabled" [
+    self.nixosModules.router-firewall
+    self.nixosModules.router-zones
+    {
+      services.router-firewall.enable = false;
+      services.router-zones = {
+        enable = true;
+        zones.lan.interfaces = [ "lan0" ];
+      };
+    }
+  ];
+
+  router-zones-sanitization-collision-fails = eval.mkNixosEvalFailureCheck "router-zones-sanitization-collision" [
+    self.nixosModules.router-firewall
+    self.nixosModules.router-zones
+    {
+      services.router-firewall.enable = true;
+      services.router-zones = {
+        enable = true;
+        zones = {
+          "lan.0".interfaces = [ "eth1" ];
+          "lan-0".interfaces = [ "eth2" ];
+        };
+      };
+    }
+  ];
+
+  router-zones-sanitization-eval = eval.mkNixosEvalCheck "router-zones-sanitization" [
+    self.nixosModules.router-firewall
+    self.nixosModules.router-zones
+    {
+      services.router-firewall = {
+        enable = true;
+        wanInterfaces = [ "wan0" ];
+      };
+      services.router-zones = {
+        enable = true;
+        zones = {
+          wan.interfaces = [ "wan0" ];
+          "lan.0".interfaces = [ "lan0" ];
+        };
+        policies = [
+          {
+            fromZone = "lan.0";
+            toZone = "wan";
+            action = "reject";
+          }
+        ];
+      };
+    }
+    ({ config, ... }: {
+      assertions = [
+        {
+          assertion = lib.hasInfix ''chain zone_lan-0_forward'' config.networking.nftables.ruleset;
+          message = "router-zones should sanitize special characters in chain definitions.";
+        }
+        {
+          assertion = lib.hasInfix ''jump zone_lan-0_forward'' config.networking.nftables.ruleset;
+          message = "router-zones should sanitize special characters in jump rules.";
+        }
+        {
+          assertion = lib.hasInfix ''oifname { "wan0" } reject comment "router-zones lan.0->wan"'' config.networking.nftables.ruleset;
+          message = "router-zones should render reject policy actions.";
+        }
+        {
+          assertion = lib.hasInfix ''return comment "router-zones default for lan.0"'' config.networking.nftables.ruleset;
+          message = "router-zones should preserve the default return action.";
+        }
+      ];
+    })
+  ];
 }
